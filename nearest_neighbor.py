66b7ba556439cdf4ff73c3a03beb0408520c2c24
denise <22denise.lie@gmail.com>
2025-03-30 01:40:15 +0800

    nearest_neighbor

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -4,7 +4,7 @@ import itertools
 """
 To use this file with example testcases, run: 
 
-python vrp.py < 1.in > 1.out
+python nearest_neighbor.py < 1.in > 1.out
 
 This reads input from 1.in and prints output to 1.out. 
 """
@@ -26,6 +26,37 @@ def solve_cvrp(n, Q, D, q):
     """TODO: Solve the Capacitated Vehicle Routing Problem and return a list of routes."""
     routes = [[0]]
 
+    unvisited = set(range(1, n))
+
+    while unvisited:
+        
+        route = [0] # Start a new route from the depot
+        capacity = Q
+        current = 0
+
+        # Find nearest unvisited
+        while unvisited: 
+            nearest = None
+            nearest_dist = float('inf')
+
+            for customer in unvisited: 
+                if q[customer] <= capacity and D[current][customer] < nearest_dist:
+                    nearest = customer
+                    nearest_dist = D[current][customer]
+            
+            # No more customers can fit, return to depot
+            if nearest is None:
+                break
+
+            # Visit nearest customer
+            route.append(nearest)
+            capacity -= q[nearest]
+            unvisited.remove(nearest)
+            current = nearest
+        
+        route.append(0) # return to depot
+        routes.append(route)
+    
     return routes
 
 def check(routes, n, Q, D, q):
